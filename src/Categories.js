2cf90582b826245e0d83b4c3171254d28aafde17
Chesseszzy <46335633+Chesseszzy@users.noreply.github.com>
2022-05-26 08:26:00 +0100

    Add files via upload

---
+++ src/Categories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+//  these components is for the buttons so the more categories you have the more buttons
+
+const Categories = ({ categories, filterItems }) => {
+    return (
+        <div className='btn-container'>
+            {categories.map((category, index) => {
+                return (
+                    <button 
+                        type="button"
+                        className="filter-btn"
+                        key={index}
+                        onClick = {() => filterItems (category)}>
+                            {category}
+                        </button>
+                );
+            }
+            )}
+        </div>
+    );
+
+
+};
+
+
+
+
+
+export default Categories;
